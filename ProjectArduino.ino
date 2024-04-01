b4a11f5bba55aa95c3883b3c10bbfbcb0aca0f74
Roger Tsai <ktpss101177@gmail.com>
2024-04-01 22:32:50 +0800

    dht project 1.1
    重寫腳位定義

--- ProjectArduino.ino
+++ ProjectArduino.ino
@@ -1,67 +1,68 @@
-#include "DHT.h"
-#include <LiquidCrystal.h>
+#include <DHT.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
-#define DHTPIN 2          // DHT11連接到Arduino的數位引腳2
-#define DHTTYPE DHT11     // 使用的DHT型號
-#define FAN_PIN 3         // 風扇連接到引腳3
-#define MIST_PIN 4        // 水霧化器連接到引腳4
-#define LCD_RS 7          // LCD的RS接腳
-#define LCD_EN 8          // LCD的EN接腳
-#define LCD_D4 9          // LCD的D4接腳
-#define LCD_D5 10         // LCD的D5接腳
-#define LCD_D6 11         // LCD的D6接腳
-#define LCD_D7 12         // LCD的D7接腳
+#define DHTPIN 3        // DHT11 感測器連接至 Arduino 的腳位 3
+#define DHTTYPE DHT11   // 使用 DHT11 感測器
+
+#define RELAY_PIN 5     // 繼電器連接至 Arduino 的腳位 5
+#define FAN_PIN RELAY_PIN // 風扇連接至繼電器的腳位，與繼電器相同
+#define MIST_PIN 4      // 水霧化器連接至 Arduino 的腳位 4
+
+#define LCD_ADDR 0x27   // I2C 介面 LCD 的地址
+#define LCD_COLS 16     // LCD 的列數
+#define LCD_ROWS 2      // LCD 的行數
 
 DHT dht(DHTPIN, DHTTYPE);
-LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
+
+// 創建 I2C 介面的 LCD 物件
+LiquidCrystal_I2C lcd(LCD_ADDR, LCD_COLS, LCD_ROWS);
 
 void setup() {
-  Serial.begin(9600);
-  dht.begin();
+  pinMode(RELAY_PIN, OUTPUT);
   pinMode(FAN_PIN, OUTPUT);
   pinMode(MIST_PIN, OUTPUT);
-  lcd.begin(16, 2);
-  lcd.clear();
+  dht.begin();
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Humidity:                ");
+  lcd.setCursor(0, 1);
+  lcd.print("Temperature:             ");
 }
 
 void loop() {
-  // 讀取溫度和濕度
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
 
-  // 檢查讀取是否成功
-  if (isnan(h) || isnan(t)) {
-    Serial.println("Failed to read from DHT sensor!");
-    lcd.clear();
-    lcd.print("Read Error!");
-    return;
+  // if (isnan(humidity) || isnan(temperature)) {
+  //   lcd.clear();
+  //   lcd.print("Error reading data.");
+  //   delay(2000); 
+  // }
+
+  lcd.setCursor(10, 0);
+  lcd.print(humidity);
+  lcd.print("                         ");
+  lcd.setCursor(13, 1);
+  lcd.print(temperature);
+  lcd.print("                         ");
+
+  // 檢查是否需要加濕 標準我亂訂的
+  if (humidity < 60 || temperature > 26) { 
+    digitalWrite(RELAY_PIN, HIGH);
+    lcd.setCursor(0, 0);
+    lcd.print("Humidifying…");
+    delay(5000); 
+    digitalWrite(MIST_PIN, HIGH); 
+    digitalWrite(FAN_PIN, HIGH); 
   }
-
-  // 顯示溫度和濕度到LCD
-  lcd.setCursor(0, 0);
-  lcd.print("Hum:");
-  lcd.print(h);
-  lcd.print("% Temp:");
-  lcd.print(t);
-  lcd.print((char)223); // 顯示攝氏度符號
-  lcd.print("C");
-
-  // 判斷是否過熱
-  if(t > 30 && h < 50) {
-    Serial.println("Overheat detected, activating mister and fan.");
-    lcd.setCursor(0, 1);
-    lcd.print("Overheat! Cooling..");
-    digitalWrite(MIST_PIN, HIGH);
-    delay(1000);
+  else {
+    digitalWrite(RELAY_PIN, LOW); 
     digitalWrite(MIST_PIN, LOW);
-    digitalWrite(FAN_PIN, HIGH);
+    digitalWrite(FAN_PIN, LOW);
+    lcd.setCursor(0, 0);
+    lcd.print("0_0");
     delay(5000);
-    digitalWrite(FAN_PIN, LOW);
-  } else {
-    // 不過熱時關閉水霧化器和風扇
-    digitalWrite(MIST_PIN, LOW);
-    digitalWrite(FAN_PIN, LOW);
   }
-
-  delay(2000); // 每2秒讀取一次
 }
